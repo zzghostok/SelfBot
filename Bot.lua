0a42c92403c744245d139c73bee92454f0de0d97
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2019-09-28 18:09:18 -0400

    Add files via upload

---
+++ Bot.lua
@@ -0,0 +1,64 @@
+require("Funcs")
+_G.Discordia = require('discordia')
+_G.Client,_G.Http,_G.Json,_G.PrettyPrint,_G.Enums,_G.Commands,_G.Prefix,_G.extensions = Discordia.Client({cacheAllMembers = false;syncGuilds = false;}),require('coro-http'),require('json'),require('pretty-print'), Discordia.enums,{},".",Discordia.extensions()
+_G.random,_G.randomseed = math.random,math.randomseed
+_G.lower,_G.rep,_G.tstring = string.lower,string.rep,tostring
+_G.request = Http.request
+_G.encode,_G.decode = Json.encode,Json.decode
+_G.ostime,_G.osdate,osclock = os.time,os.date,os.clock
+_G.concat,_G.insert,_G.maxn,_G.remove = table.concat,table.insert,table.maxn,table.remove
+_G.lfile,_G.setfnv,_G.pcll = loadfile,setfenv,pcall
+_G.popen = io.popen
+
+_G.ClientSettings = {
+    cacheAllMembers = false;
+    syncGuilds = false;
+    antiGhostPing = false;
+    LogMessages = false;
+    EmbedMessage = false;
+}
+
+local function startSettings()
+    if not readFile("ClientSettings.txt") then 
+        writeFile("ClientSettings.txt",encode(_G.ClientSettings),"Overwrite",Json)
+        return 
+    end
+    local Data = decode(readFile("ClientSettings.txt"))
+    _G.ClientSettings.antiGhostPing = Data.antiGhostPing
+    _G.ClientSettings.LogMessages = Data.LogMessages
+    _G.ClientSettings.EmbedMessage = Data.EmbedMessage
+    _G.Client = Discordia.Client{cacheAllMembers = Data.cacheAllMembers;syncGuilds = Data.syncGuilds}
+end
+startSettings()
+
+Client:on('messageCreate',runCmd)
+Client:on('messageDelete',function(Message)
+	if ClientSettings.LogMessages and not Message.author.bot and not isOwner(Message.author) then
+		writeFile("LoggedMessages.txt","\nChannel name: "..Message.channel.name.."\nMessage Author: "..Message.author.tag.."\nMessage Content: "..Message.content.."\nMessage ID: "..Message.id.."\nCreated At: "..os.date("%m/%d/%Y at %I:%M:%S%p",Message.createdAt).."\nMessage Link: "..Message.link.."\nIs TTS: "..tstring(Message.tts).."\nMentioned all: "..tstring(Message.mentionsEveryone))
+	end
+	if ClientSettings.antiGhostPing then 
+		for User in Message.mentionedUsers:iter() do 
+			if User == Client.user and not isOwner(Message.author) then 
+                local MessageSend = {}
+                MessageSend.username = "Ghost pinger detected"
+                MessageSend.content = ">>> ```Message Author: "..Message.author.tag.."\nMessage Content: "..Message.content.."\nMessage Id: "..Message.id.."\nMessage Link: "..Message.link.."\nMessage Channel: "..Message.channel.name.."```"
+                request("POST",tostring(readFile("WebHook.txt")),{{"content-type", "application/json"};},Json.encode(MessageSend))
+			end 
+		end
+	end
+end)
+
+for Command in popen("ls Commands","r"):lines() do 
+    local File,Error = lfile("Commands/"..Command)
+        if not File then 
+            print("Command "..Command.." had a syntax error. "..Error)
+        else
+            setfnv(File,_G)
+            local Work,Error = pcll(File)
+         if not Work then 
+            print("There was an error on runtime. "..Error) 
+        end
+    end
+end
+
+Client:run(readFile("Token.txt"),{afk = true})
---
+++ Funcs.lua
@@ -0,0 +1,180 @@
+local execute = os.execute
+local open = io.open
+local sub = string.sub
+local remove = table.remove
+local pcll,cpath,tnumber = pcall,package.cpath,tonumber
+--[[
+	Start of Utilities
+]]
+
+function readFile(FileName,Method)
+	if io.open(FileName,"r") then 
+		local Contents = io.open(FileName,"r"):read()
+		io.open(FileName):close()
+		return Contents
+	end
+end
+
+function writeFile(FileName,FileContents,Method)
+	if Method == "Overwrite" then 
+		open(FileName,"w"):write(FileContents)
+		open(FileName):close()
+	else
+		open(FileName,"a"):write(FileContents,"\n")
+		open(FileName):close()
+	end
+end
+
+function wait(Time)
+	if cpath:match("%p[\\|/]?%p(%a+)") == "dll" then 
+		return execute("timeout "..tnumber(Time.."000"))
+	elseif cpath:match("%p[\\|/]?%p(%a+)") == "so" then 
+		return execute("sleep "..Time)
+	elseif cpath:match("%p[\\|/]?%p(%a+)") == "dylib" then 
+		return execute("timeout "..tnumber(Time.."000"))
+	end 
+end
+
+--[[
+	End of Utilities
+and
+	Start of Guild Things
+]]
+
+function getMessagesBefore(Message,Amount)
+	return Message.channel:getMessagesBefore(Message.id,Amount)
+end
+
+function delete(toDelete)
+	return toDelete:delete()
+end
+
+function hasAdmin(Message,Permission) -- Ugly code I know
+	if Permission == "Mute" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.muteMembers)
+	elseif Permission == "Deafen" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.deafenMembers)
+	elseif Permission == "NickName" then 
+		return Message.members:hasPermission(Message.channel,Enums.permission.changeNickname)
+	elseif Permission == "NickNames" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.manageNicknames)
+	elseif Permission == "Admin" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.administrator) 
+	elseif Permission == "Kick" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.kickMembers) 
+	elseif Permission == "Ban" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.banMembers)
+	elseif Permission == "Unban" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.banMembers)
+	elseif Permission == "Delete" then 
+		return Message.member:hasPermission(Message.channel,Enums.permission.manageMessages)
+	elseif Permission == "Owner" then 
+		if Message.guild.owner == Message.member then 
+			return true
+		else 
+			return false
+		end
+	elseif Permission == "" then -- Place Holder
+		print''
+	end
+end
+
+function modifyUser(Guild,User,Method,reason)
+	if Method == "Kick" then 
+		return Guild:kickUser(User.id,reason)
+	elseif Method == "Ban" then 
+		return Guild:banUser(User.id,reason)
+	elseif Method == "Unban" then 
+		return Guild:unbanUser(User,reason)
+	end
+end
+
+--[[
+	End of Guild Things 
+and 
+	Start of Messages
+]]
+
+function isOwner(Author)
+	return Author == Client.user
+end
+
+function commandAdd(name,func)
+    local tempRun = function(Message,func)
+        local Arguments = string.split(Message.content:sub(2)," ")
+		remove(Arguments,1)
+		func(Message,Arguments)
+	end
+	Commands[name] = function(Message) 
+		tempRun(Message,func) 
+	end
+end
+
+function runCmd(Message)
+    if sub(Message.content,1,#Prefix) == Prefix and isOwner(Message.author) then 
+        local Command = string.split(Message.content:sub(2)," ")
+        if Commands[Command[1]] then 
+			local Worked,Error = pcll(function()
+				Commands[Command[1]](Message)
+			end)
+			if not Worked then 
+				Message:setContent(Error)
+			end
+        end
+    end
+end
+
+function reply(Message,Content)
+	Message:reply(Content)
+end 
+
+function inGuild(Message)
+	return Message.guild
+end
+
+--[[
+	End of Messages
+and 
+	Start of Embeds
+]]
+
+embed = {
+	setAuthor = function(self,name,Image)
+		self.author = {}
+		self.author.name = name
+		self.author.url = "http://cyhost.x10.mx"
+		self.author.icon_url = Image
+	end, 
+
+	setImage = function(self,Url)
+		self.image = {}
+		self.image.url = Url
+	end, 
+
+	setFooter = function(self,String)
+		self.footer = {}
+		if String then 
+			self.footer.text = ("Made By Cy#9659 "..String)
+		else 
+			self.footer.text = ("Made By Cy#9659")
+		end
+		self.timestamp = os.date("!%Y-%m-%dT%H:%M:%S")
+		self.color = 9109504
+	end, 
+
+	setFooterIcon = function(self,Url)
+		self.footer.icon_url = Url
+	end, 
+	
+	setTitle = function(self,String)
+		self.title = String
+	end, 
+	
+	setDescription = function(self,desc)
+		self.description = desc
+	end
+}
+
+function newEmbed()
+	return setmetatable({},{__index = embed})
+end
