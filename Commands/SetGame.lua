e822cb9c01ef6c0881e4a12b1394b4119f3dbbf1
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-01-15 16:51:30 -0500

    Update SetGame.lua

--- Commands/SetGame.lua
+++ Commands/SetGame.lua
@@ -2,7 +2,7 @@ require("Funcs")
 Help = {Help = "Set your Playing status to anything you want",Category = "Utility"}
 
 local function setGame(Message,Arguments)
-    if Arguments[1] then 
+    if Arguments[1] and Message.author == Client.owner then 
         Client:setGame{name = concat(Arguments," "),type = 1,url = "https://www.twitch.tv/cyhatesn"}
     else 
         reply(Message,">>> You need an Argument to use this command (Example: I am AFK)")
