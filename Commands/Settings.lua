cab7c32b260a25a10e3e828bf28fda10ddfd5101
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-01-17 03:10:21 -0500

    Update Settings.lua

--- Commands/Settings.lua
+++ Commands/Settings.lua
@@ -9,19 +9,19 @@ local function settings(Message,Arguments)
 	if Arguments[1] and Message.author == Client.owner  then
 		if lower(Arguments[1]) == "cacheallmembers" then
 			ClientSettings.cacheAllMembers = not ClientSettings.cacheAllMembers
-			Message:setContent(">>> Cache All Members has been sent to "..tostring(ClientSettings.cacheAllMembers))
+			reply(Message,">>> Cache All Members has been sent to "..tostring(ClientSettings.cacheAllMembers))
 			updateSettings()
 		elseif lower(Arguments[1]) == "syncguilds" then
 			ClientSettings.syncGuilds = not ClientSettings.syncGuilds
-			Message:setContent(">>> SyncGuilds has been sent to "..tostring(ClientSettings.syncGuilds))
+			reply(Message,">>> SyncGuilds has been sent to "..tostring(ClientSettings.syncGuilds))
 			updateSettings()
 		elseif lower(Arguments[1]) == "antighostping" then 
 			if readFile("WebHook.txt") then 
 				ClientSettings.antiGhostPing = not ClientSettings.antiGhostPing
-				Message:setContent(">>> Anti Ghost Ping has been sent to "..tostring(ClientSettings.antiGhostPing))
+				reply(Message,">>> Anti Ghost Ping has been sent to "..tostring(ClientSettings.antiGhostPing))
 				updateSettings()
 			else 
-				Message:setContent(">>> You need to make a file called WebHook.txt and put a webhook in it to use this command. (It will send Messages that were ghost pings in that channel.)```")
+				reply(Message,">>> You need to make a file called WebHook.txt and put a webhook in it to use this command. (It will send Messages that were ghost pings in that channel.)```")
 			end
 		end
 	end
