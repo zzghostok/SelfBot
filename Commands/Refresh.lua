19ccd0283e8431dc65861bfa5285eadf472803fc
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-01-17 03:08:23 -0500

    Update Refresh.lua

--- Commands/Refresh.lua
+++ Commands/Refresh.lua
@@ -14,18 +14,18 @@ local File,FileAmount = popen("ls Commands"),{}
 		if ReloadFile then 
 			local Work,Error = lfile("Commands/"..ReloadFile)
 			if not Work then 
-				Message:setContent(">>> Syntax Error. "..ReloadFile.."```Lua\n"..Error.."```")
+				reply(Message,">>> Syntax Error. "..ReloadFile.."```Lua\n"..Error.."```")
 			else
 				setfnv(Work,_G)
 				local Func,Error = pcll(Work)
 				if not Func then
-					Message:setContent(">>> Runtime Error. "..ReloadFile.."```Lua\n"..Error.."```")
+					reply(Message,">>> Runtime Error. "..ReloadFile.."```Lua\n"..Error.."```")
 				else
-					Message:setContent(">>> "..ReloadFile.." has been reloaded!")
+					reply(Message,">>> "..ReloadFile.." has been reloaded!")
 				end
 			end
 		else
-			Message:setContent(">>> "..Arguments[1].." is not a valid Command")
+			reply(Message,">>> "..Arguments[1].." is not a valid Command")
 		end
 	end
 end
