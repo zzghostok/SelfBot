defcd11506ace3af6c82f1df26a8e577537704ae
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-01-21 16:31:46 -0500

    Update Settings.lua

--- Commands/Settings.lua
+++ Commands/Settings.lua
@@ -1,31 +1,27 @@
 require("Funcs")
-Help = {Help = "Settings [CacheAllMembers/SyncGuilds/AntiGhostPing/canalluse]",Category = "Utility"}
+Help = {Help = "Settings [CacheAllMembers/SyncGuilds/AntiGhostPing/CanAllUse/EmbedMessages]",Category = "Utility"}
 
 local function settings(Message,Arguments)
 	if Arguments[1] and Message.author == Client.owner  then
 		if lower(Arguments[1]) == "cacheallmembers" then
       _G.ClientSettings.cacheAllMembers = not _G.ClientSettings.cacheAllMembers
-			reply(Message,">>> Cache All Members has been sent to "..tostring(_G.ClientSettings.cacheAllMembers))
-			updateSettings()
 		elseif lower(Arguments[1]) == "syncguilds" then
       _G.ClientSettings.syncGuilds = not _G.ClientSettings.syncGuilds
-      reply(Message,">>> SyncGuilds has been sent to "..tostring(_G.ClientSettings.syncGuilds))
-			updateSettings()
     elseif lower(Arguments[1]) == "canalluse" then 
           _G.ClientSettings.canAllUse = not _G.ClientSettings.canAllUse
-          reply(Message,">>> Can All Use has been sent to "..tostring(_G.ClientSettings.canAllUse))
-				  updateSettings()
+    elseif lower(Arguments[1]) == "embedmessages" then 
+		      _G.ClientSettings.EmbedMessage = not _G.ClientSettings.EmbedMessage
+    elseif lower(Arguments[1]) == "logmessages" then 
+          _G.ClientSettings.LogMessages = not _G.ClientSettings.LogMessage
 		elseif lower(Arguments[1]) == "antighostping" then 
 			if readFile("WebHook.txt") then 
 				_G.ClientSettings.antiGhostPing = not _G.ClientSettings.antiGhostPing
-				reply(Message,">>> Anti Ghost Ping has been sent to "..tostring(_G.ClientSettings.antiGhostPing))
-				updateSettings()
 			else 
 				reply(Message,">>> You need to make a file called WebHook.txt and put a webhook in it to use this command. (It will send Messages that were ghost pings in that channel.)```")
 			end
 		end
+      updateSettings()
 	end
 end
-
 
 commandAdd("settings",settings)
