8fae069baccbea5e4cda21a5bffc8e3bebecb490
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-01-21 15:55:42 -0500

    This is still fucking gay.
    Made it so it's locked to NSFW channels or Dms / Group Dms (I'm looking at you people who spammed it in !fishgang)

--- Commands/degen.lua
+++ Commands/degen.lua
@@ -2,14 +2,14 @@ require("Funcs")
 Help = {Help = "Gives you your gay hentai you so dearly love.",Category = "Nsfw"}
 
 local function sendGayTai(Message,Arguments)
-  if not Arguments[1] then 
+  if not Arguments[1] and inGuild(Message) and Message.channel.nsfw or not inGuild(Message) and not Arguments[1] then 
 		local _,Body = request("GET","https://nekos.life/api/v2/img/lewd")
       local Embed = newEmbed()
       Embed:setAuthor("CyBot","https://i.imgur.com/WM6c9ld.png")
       Embed:setFooter()
       Embed:setImage(decode(Body)['url'])
       reply(Message,{embed = Embed})
-  elseif Arguments[1] and lower(Arguments[1]) == "bomb" then
+  elseif Arguments[1] and lower(Arguments[1]) == "bomb" and inGuild(Message) and Message.channel.nsfw or not inGuild(Message) then
     local HenTable = {}
     for i = 1,5 do
       local _,Body = request("GET","https://nekos.life/api/v2/img/lewd")
