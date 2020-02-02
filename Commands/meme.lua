40dfe8f662eb47f859ffefb3cc6355206955640d
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-02-02 17:43:42 -0500

    Made meme bomb faster / picks more subreddits

--- Commands/meme.lua
+++ Commands/meme.lua
@@ -1,16 +1,13 @@
 require("Funcs")
 Help = {Help = "Gives you bad memes from soyreddit",Category = "Fun"}
-
+local MemeTable = {"dankmemes","NewOffensiveMemes","darkmeme","memes","meirl"}
 local function soyMeme(Message,Arguments)
   if Arguments[1] and Arguments[1]:lower() == "bomb" then 
-    local MemeTable = {}
-    for i = 1,5 do
-    local _,Body = request("GET","https://meme-api.herokuapp.com/gimme/dankmemes")
-      insert(MemeTable,decode(Body)['url'])
-    end
-      reply(Message,concat(MemeTable," "))
-  else 
-      local _,Body = request("GET","https://meme-api.herokuapp.com/gimme/dankmemes")
+    local _,Body = request("GET","https://meme-api.herokuapp.com/gimme/5")
+    local New = decode(Body)
+      reply(Message,New['memes'][1]['url'].." "..New['memes'][2]['url'].." "..New['memes'][3]['url'].." "..New['memes'][4]['url'].." "..New['memes'][5]['url'])
+  else
+      local _,Body = request("GET","https://meme-api.herokuapp.com/gimme/"..MemeTable[math.random(1,#MemeTable)])
       Body = decode(Body)
       local Embed = newEmbed()
       Embed:setAuthor("CyBot","https://i.imgur.com/WM6c9ld.png")
